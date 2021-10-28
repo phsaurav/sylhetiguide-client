22e3f657a982c48ae4f767f2d14b7fa24105d30c
PH Saurav <phsaurav29@gmail.com>
2021-10-29 01:29:55 +0600

    Tailwind Added 🎨

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<h1>This is Working</h1>
+			<h1 className="text-10xl">This is Working</h1>
 		</div>
 	);
 }
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '1080px',
+			xl: '1440px',
+		},
+		colors: {
+			green: {
+				10: '#F2F3F5',
+				20: '#6A87C1',
+				30: '#5973B0',
+				40: '#2F4171',
+				50: '#222e52',
+				...colors.green,
+			},
+			black: colors.black,
+			white: colors.white,
+			gray: colors.trueGray,
+			bluegray: colors.blueGray,
+			indigo: colors.indigo,
+			red: colors.rose,
+			yellow: colors.amber,
+			orange: colors.orange,
+			blue: colors.blue,
+			lime: colors.lime,
+			brand: {
+				1: '#CD4760',
+				2: '#CD445D',
+				3: '#C1344C',
+			},
+		},
+		fontFamily: {},
+		fontSize: {
+			xs: '.75rem',
+			sm: '.875rem',
+			tiny: '.875rem',
+			base: '1rem',
+			lg: '1.125rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '5rem',
+			'8xl': '7rem',
+			'9xl': '9rem',
+			'10xl': '12rem',
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};

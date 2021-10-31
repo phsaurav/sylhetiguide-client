01005c1078bb6b7fa37c9866796687d8c1bb99d6
PH Saurav <phsaurav29@gmail.com>
2021-10-31 19:50:36 +0600

    404 Page Updated 🚫

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../assets/404.png';
 
 const NotFound = () => {
 	return (
 		<div>
-			<h1>404 Page Not Found!!</h1>
+			<div className="h-screen w-full flex flex-col items-center justify-center bg-teal-lightest font-sans">
+				<div
+					v-if="modal.visible"
+					className="h-screen w-full absolute flex items-center justify-center bg-modal mb-40"
+				>
+					<div className="bg-white rounded shadow p-8 m-4 max-w-lg max-h-full text-center ">
+						<img src={img} alt="404 Images" />
+						<h1
+							className="text-center text-3xl font-bold text-blue-40 py-5  uppercase"
+							id="services"
+						>
+							Page Not Found
+						</h1>
+						<Link to="/home">
+							<div className=" mt-3  flex  flex-row justify-center">
+								<button
+									className=" p-2  w-40  bg-brand-1  rounded-lg  text-white hover:bg-brand-3"
+									type="button"
+								>
+									Go Back
+								</button>
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
